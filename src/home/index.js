1ca3e5efc64dd6b4264d309036f32551b37b2daa
Warllei Martins <warllei.s.martins@hotmail.com>
2022-05-25 19:19:04 -0300

    first-commit

---
+++ src/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Asdasdasd</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/view/header/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Cloud from "../../img/cloud.png"
+import "./style.scss"
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-content">
+                <ul className="login-content">
+                    <li><a href="#">Entrar</a></li>
+                    <li className="new-batFriend"><a href="#">Assine Agora</a></li>
+                </ul>
+                <div className="title">
+                    <h1>BATMAN</h1>
+                </div>
+                <div class="cloud">
+                    <img src={Cloud} alt="" class="img_cloud" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
